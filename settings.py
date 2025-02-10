46e6742e89f7b2a0945a1b28d636025984571f02
Bebrik-blin <lovehueta@gmail.com>
2025-02-10 23:27:55 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,7 @@
+drinks = ('прозрачная вода', 'зеленая вода', 'рыжая вода')
+drinks_and_message_coeds = {'прозрачная вода' : '1', 'зеленая вода' : '2', 'рыжая вода': '3'}
+max_glases_count = 4
+empty_glass_value = 0
+order_prefix = 'order:'
+
+RESIZE_INTERFEISE = False
